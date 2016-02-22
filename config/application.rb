338bc097f2f34cc1043120f74fcf451b7deb0879
Bill Fulbright <bill@sdetfoundry.com>
2016-02-22 13:13:48 -0500

    update to add ruby '2.2.4' to Gemfile

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-ruby 'ruby 2.2.3p173'
+ruby "2.2.4"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
